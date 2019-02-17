a492571b566406846402c122933e5c3dbafd21cb
Your Name <you@example.com>
2019-02-16 20:43:01 -0400

    Step03

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ class App extends Component {
     // Use these state variables to save the user comment and store a list of previously added comments
     this.state = {
       comment: '',
+      author: 'Sha',
+      image: "https://api.adorable.io/avatars/285/avatar_user_4.png",
       messages: [],
       account: null,
       web3: null,
@@ -29,7 +31,13 @@ class App extends Component {
 
   componentDidMount = async () => {
 
-    setInterval(async () => {
+
+    this.setState({transferForm:{
+      amount: 100,
+      address:'0xDFFD5f1D232dD91Be8D15598C048999C2147ae75'
+    }})
+
+    //setInterval(async () => {
 
       try {
         //Does the browser provide access to web3? (this is provided via Metamask in my case)
@@ -56,7 +64,7 @@ class App extends Component {
         this.setState({ errorMsg: 'Could not detect web3' })
       }
 
-    }, 1000)
+   // }, 1000)
 
 
   }
@@ -68,16 +76,58 @@ class App extends Component {
     //Instantiate the polyToken smart contract
     //***TODO: Grab deployed contract address from commandline */
     const tutorialInstance = new web3.eth.Contract(TutorialToken.abi, '0x7aa77209196c6ea244c36c0351d448110a39e07d')
-
-    console.log(tutorialInstance)
+    const balance = await tutorialInstance.methods.balanceOf(accounts[0]).call({ from: accounts[0] })
+    console.log(balance)
     //We use web3.utils.fromWei to display the units of the balance from wei to ether
-    this.setState({ contractInstance: tutorialInstance })
+    this.setState({ web3, contractInstance: tutorialInstance })
 
   }
 
   handleSubmit = () => {
+
+    var date = new Date()
+
+    var messagetime = date.toLocaleString()
+
     console.log(this.state.comment)
+    console.log(this.state.author)
+
+    const {messages} = this.state
+
+    messages.push({
+      messages_value: this.state.comment,
+      messages_author: this.state.author,
+      messages_image: this.state.image,
+      messages_time: messagetime
+    });
+
+    this.setState({messages:messages})
+
+    //console.log(this.state.comment)
     // Modify this function to handle user submissions and update state
+  }
+
+  handleDonate = (e)=>{
+    e.preventDefault()
+    const {account,balance, errorMsg, transferForm, contractInstance, decimals, web3} = this.state
+
+    if(transferForm.account < 1){
+      return
+    }
+
+    contractInstance.methods.transfer(transferForm.address, web3.utils.toWei(transferForm.amount.toString(), 'ether')).send({ from: account,gas: 1000000 })
+      .then((result) => {
+
+        console.log(result)
+
+        this.setState({transferForm:{
+          amount: 0,
+          address:''
+        },})
+
+      })
+
+  
   }
 
   handleInputChange(e) {
@@ -94,6 +144,37 @@ class App extends Component {
   }
 
   render() {
+
+    //<button type="button" onClick={this.handleDonate}>Submit</button>
+
+    const {messages} = this.state
+
+    const messageTable = messages.map(value => {
+
+        return(
+      <li className="comment user-comment">
+
+            <div className="info">
+              <a href="#">{value.messages_author}</a>
+              <span>{value.messages_time}</span>
+
+            </div>
+
+            <a className="avatar" href="#">
+              <img src={value.messages_image} width="35"/>
+            </a>
+
+            <p>{value.messages_value}</p>
+
+
+        <button className="donate" onClick={(e) => this.handleDonate(e)}>Donate</button>           
+
+          </li>
+        )
+
+    })
+
+
     return (
       <div classNameName="App">
         <ul className="comment-section">
@@ -115,16 +196,17 @@ class App extends Component {
             <p>Random comment goes here</p>
 
           </li>
-
+{messageTable}
           <li className="write-new">
 
             <form>
 
-              <textarea placeholder="Write your comment here" name="comment" value={this.state.comment}></textarea>
+              <textarea placeholder="Write your comment here" name="comment" onChange={evt => this.handleInputChange(evt)}></textarea>
 
               <div>
                 <img src="https://api.adorable.io/avatars/285/avatar_user_4.png" width="35" alt="Profile of Bradley Jones" title="Bradley Jones" />
                 <button type="button" onClick={this.handleSubmit}>Submit</button>
+                <button type="button" onClick={(e) => this.handleDonate(e)}>Donate</button> 
               </div>
 
             </form>
